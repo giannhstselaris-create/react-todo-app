cf04bbed280316ce16fede6280e8d9c4597ec3a6
Ioannis Tselaris <giannhstselaris@gmail.com>
2026-05-21 23:20:08 +0300

    feat(ui): add Button shared component

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -2,3 +2,11 @@ export interface LayoutProps {
     children: React.ReactNode;
     addClasses?: string;
 }
+
+export type ButtonProps = {
+    onClick?: () => void;
+    label: string;
+    disabled?: boolean;
+    addClasses?: string;
+}
+
